7b092e2df4286e24a5e5d2d10076a2f0a351f51a
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-20 00:48:34 +0000

    Create remove_Nth_node_from_end_of_list.java

---
+++ remove_Nth_node_from_end_of_list.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        // Skip the desired node
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+}
