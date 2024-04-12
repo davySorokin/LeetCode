ebe378a948fe719f42508ff73aecc99b25cac5b8
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-04-12 12:34:04 +0100

    binary_tree_level_order_traversal.java

---
+++ binary_search_BFS/binary_tree_level_order_traversal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<>();
+            for(int i = 0; i < levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            result.add(subList);
+        }
+        
+        return result;
+    }
+}
