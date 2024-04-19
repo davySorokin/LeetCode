dc2cd0e7ded1c76d924b0c11073ac4d75efd6f18
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-04-19 19:39:22 +0100

    design_add_&_search_words_data_structure.py

---
+++ Trie/design_add_&_search_words_data_structure.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        def search_in_node(word, node):
+            for i, char in enumerate(word):
+                if char == '.':
+                    for child in node.children.values():
+                        if search_in_node(word[i+1:], child):
+                            return True
+                    return False
+                else:
+                    if char not in node.children:
+                        return False
+                    node = node.children[char]
+            return node.is_end_of_word
+        
+        return search_in_node(word, self.root)
