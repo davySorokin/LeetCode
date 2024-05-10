ec198b554347d408561f23a0429ac040761e3181
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-05-10 14:04:32 +0100

    k^th_largest_element_in_array.go

---
+++ heap/k^th_largest_element_in_array.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] } // Min-heap
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// findKthLargest returns the kth largest element in the nums array
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+	for _, num := range nums {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return (*h)[0] // The root element of the min-heap is the kth largest
+}
