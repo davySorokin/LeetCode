2cd7b7d711a97ecf56b0c6775c8984d36b326f83
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-05-21 23:04:09 +0100

    single_number-2.go

--- bit_manipulation/single_number-2.go
+++ bit_manipulation/single_number-2.go
@@ -3,15 +3,17 @@ package main
 import "fmt"
 
 func singleNumber(nums []int) int {
-    var result int
+    result := 0
     for i := 0; i < 32; i++ {
         sum := 0
         for _, num := range nums {
             sum += (num >> i) & 1
         }
-        if sum % 3 != 0 {
-            result |= (1 << i)
-        }
+        result |= (sum % 3) << i
+    }
+    // Interpreting the result as a signed 32-bit integer
+    if result >= 1<<31 {  // If the result is negative in two's complement
+        result -= 1 << 32
     }
     return result
 }
