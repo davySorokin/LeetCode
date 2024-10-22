3bd6b7f25ccae79215dd78112b91164aa4198465
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2024-10-22 16:47:00 +0900

    permutation2.go
    returning 2-dimensional array (or "slice of slices")

---
+++ permutation2.go
@@ -0,0 +1,28 @@
+import (
+	"sort"
+)
+
+func permuteUnique(nums []int) [][]int {
+	var res [][]int
+	sort.Ints(nums) // Here -> Sort to ensure we can skip duplicates
+	backtrack(nums, []int{}, &res, make([]bool, len(nums)))
+	return res
+}
+
+func backtrack(nums []int, path []int, res *[][]int, used []bool) {
+	if len(path) == len(nums) {
+		temp := make([]int, len(path))
+		copy(temp, path)
+		*res = append(*res, temp)
+		return
+	}
+	
+	for i := 0; i < len(nums); i++ {
+		if used[i] || (i > 0 && nums[i] == nums[i-1] && !used[i-1]) {
+			continue
+		}
+		used[i] = true
+		backtrack(nums, append(path, nums[i]), res, used)
+		used[i] = false
+	}
+}
