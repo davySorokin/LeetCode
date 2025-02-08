a544b3a282009731b333d7e64129041f013fb623
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-02-08 14:07:20 +0000

    nextGreaterElement1.py

---
+++ nextGreaterElement1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        next_greater = {}
+        stack = []
+        
+        for num in nums2:
+            while stack and stack[-1] < num:
+                smaller_num = stack.pop()
+                next_greater[smaller_num] = num
+            stack.append(num)
+        
+        while stack:
+            next_greater[stack.pop()] = -1
+        
+        return [next_greater[num] for num in nums1]
