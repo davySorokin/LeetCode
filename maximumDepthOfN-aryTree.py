d326354acc3edf95caddf13d427090309f0e1e89
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-02-27 13:52:08 +0000

    maximumDepthOfN-aryTree.py

---
+++ maximumDepthOfN-aryTree.py
@@ -0,0 +1,11 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        return 1 + max((self.maxDepth(child) for child in root.children), default=0)
