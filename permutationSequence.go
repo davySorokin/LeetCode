3af4bb8618c409d990672da01052a6a238dfa8fa
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-07-12 10:20:32 +0100

    permutationSequence.go

---
+++ permutationSequence.go
@@ -0,0 +1,24 @@
+import (
+    "strconv"
+)
+
+func getPermutation(n int, k int) string {
+    factorial := 1
+    nums := []int{}
+    
+    // prepare factorial and number list
+    for i := 1; i <= n; i++ {
+        factorial *= i
+        nums = append(nums, i)
+    }
+
+    for i := n; i >= 1; i-- {
+        factorial /= i
+        index := k / factorial
+        res += strconv.Itoa(nums[index])
+        nums = append(nums[:index], nums[index+1:]...)
+        k %= factorial
+    }
+
+    return res
+}
