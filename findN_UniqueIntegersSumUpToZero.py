d72f842d43558b81bdb0c9293facd27beccaf055
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-09-07 09:55:41 +0100

    findN_UniqueIntegersSumUpToZero.py

---
+++ findN_UniqueIntegersSumUpToZero.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+        for i in range(1, n // 2 + 1):
+            res.extend([-i, i])
+        if n % 2 == 1:
+            res.append(0)
+        return res
