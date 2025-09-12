339aff9522ddac3264ae5c666ab08e97046cb6de
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-09-12 10:32:57 +0100

    vowelsGameInAString.py

--- vowelsGameInAString.py
+++ vowelsGameInAString.py
@@ -1,5 +1,3 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        vowels = set("aeiou")
-        vowel_count = sum(1 for c in s if c in vowels)
-        return vowel_count % 2 == 1
+        return any(ch in "aeiou" for ch in s)
