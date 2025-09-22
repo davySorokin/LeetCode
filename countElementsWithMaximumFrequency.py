258300e5eb3bfe05727cad382705e93ba76c1641
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-09-22 12:49:47 +0100

    countElementsWithMaximumFrequency.py

---
+++ countElementsWithMaximumFrequency.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def maxFrequencyElements(self, nums):
+        freq = Counter(nums)
+        max_freq = max(freq.values())
+        return sum(count for count in freq.values() if count == max_freq)
