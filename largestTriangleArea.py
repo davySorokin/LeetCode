6e0f125a520492408494b1589205f70c3ffd2959
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-09-27 09:55:43 +0100

    largestTriangleArea.py

---
+++ largestTriangleArea.py
@@ -0,0 +1,16 @@
+from typing import List
+from itertools import combinations
+
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        def area(p1, p2, p3):
+            return abs(
+                p1[0]*(p2[1] - p3[1]) +
+                p2[0]*(p3[1] - p1[1]) +
+                p3[0]*(p1[1] - p2[1])
+            ) / 2
+
+        max_area = 0
+        for p1, p2, p3 in combinations(points, 3):
+            max_area = max(max_area, area(p1, p2, p3))
+        return max_area
