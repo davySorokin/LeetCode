968af7af53301b8d75172efb50576ef6bc03e680
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-09-28 10:13:46 +0100

    largestPerimeterTriangle.py

---
+++ largestPerimeterTriangle.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)  # largest first
+        for i in range(len(nums) - 2):
+            a, b, c = nums[i], nums[i+1], nums[i+2]
+            if b + c > a:  # triangle inequality
+                return a + b + c
+        return 0
