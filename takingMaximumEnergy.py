8aef653d8b25985add95fabfe1f96cb2cbdcd1fb
David Sorokin <96061027+davySorokin@users.noreply.github.com>
2025-10-10 10:52:43 +0100

    takingMaximumEnergy.py

---
+++ takingMaximumEnergy.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumEnergy(self, energy: list[int], k: int) -> int:
+        n = len(energy)
+        dp = [0] * n
+        
+        for i in range(n - 1, -1, -1):
+            dp[i] = energy[i]
+            if i + k < n:
+                dp[i] += dp[i + k]
+        
+        return max(dp)
